e54ceefa09fdf2d37b5596066adcc013188d8668
Fbonnefoy <Fbonnefoy@users.noreply.github.com>
2020-09-14 18:18:10 +0200

    python 3 adaptation
    Original (Python 2) can be found here

--- RyersonGeo_-_Distance_Matrix_-_Euclidean.py
+++ RyersonGeo_-_Distance_Matrix_-_Euclidean.py
@@ -16,37 +16,40 @@
 # produces a euclidean distance matrix.
 
 # Imports.
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 from qgis.core import *
 from qgis.gui import *
-
+from qgis import processing
+from processing.tools import dataobjects
+# Creating Context
+context = dataobjects.createContext()
 # Get the layers.
-lyrConsumer = processing.getObject(Consumer_Centroid_Layer)
-lyrCentre = processing.getObject(Centre_Point_Layer)
+lyrConsumer = QgsProcessingUtils.mapLayerFromString('contours-iris-2014',context)
+lyrCentre = QgsProcessingUtils.mapLayerFromString('Centroids',context)
 
 # Get the fields.
-fldConsumerID_index = lyrConsumer.fieldNameIndex(Consumer_Centroid_Layer_ID_Field)
-fldCentreID_index = lyrCentre.fieldNameIndex(Centre_Point_Layer_ID_Field)
+fldConsumerID_index = lyrConsumer.fieldNameIndex(dcomiris)
+fldCentreID_index = lyrCentre.fieldNameIndex(dcomiris)
 
 # Need to prepare output layer and add new fields.
 # New fields are simply the ID of the Centre.
 # Loop through each Centre to construct new field names.
 lyrOutput = processing.getObject(Output_Layer)
-provider = lyrOutput.dataProvider() 
+provider = lyrOutput.dataProvider()
 
 # Optimize feature request for this loop.
 request1 = QgsFeatureRequest().setFlags(QgsFeatureRequest.NoGeometry).setSubsetOfAttributes([Centre_Point_Layer_ID_Field], lyrCentre.fields() )
 
 # The loop.
 for centreFeature in lyrCentre.getFeatures(request1):
-    
+
     # Capture value of fldCentreID_index (current ID).
     currentCentreID = centreFeature[fldCentreID_index]
 
-    # Add and name the field. Double type for distances.   
-    new_field_name = currentCentreID    
+    # Add and name the field. Double type for distances.
+    new_field_name = currentCentreID
     provider.addAttributes([QgsField(new_field_name, QVariant.Double)])
     lyrOutput.updateFields()
 
@@ -55,26 +58,26 @@ lyrOutput.startEditing()
 
 # Optimize feature request for outer nested loop.
 request2 = QgsFeatureRequest().setSubsetOfAttributes([Consumer_Centroid_Layer_ID_Field], lyrConsumer.fields() )
-   
+
 # Loop through each Consumer feature.
 for consumerFeature in lyrConsumer.getFeatures(request2):
-    
+
     # Capture value of fldConsumerID_index (current ID).
     currentConsumerID = consumerFeature[fldConsumerID_index]
-    
+
     # Loop through each Centre feature.
     for centreFeature in lyrCentre.getFeatures():
-        
+
         # Capture value of fldCentreID_index (current ID).
         currentCentreID = centreFeature[fldCentreID_index]
 
         # Create a measurement object.
         mObject = QgsDistanceArea()
-        
+
         # Measure the euclidean distance.
         eDistance = mObject.measureLine(consumerFeature.geometry().asPoint(), centreFeature.geometry().asPoint())
 
-        # Set the euclidean distance value of the new Centre field for 
+        # Set the euclidean distance value of the new Centre field for
         # the current Consumer and Centre.
         current_distmatrix_field = currentCentreID
         distmatrix_field_index = lyrOutput.fieldNameIndex(current_distmatrix_field)
--- RyersonGeo_-_Distance_Matrix_-_Network.py
+++ RyersonGeo_-_Distance_Matrix_-_Network.py
@@ -18,12 +18,13 @@
 # produces a network distance matrix.
 
 # Imports.
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 from qgis.core import *
 from qgis.gui import *
-from qgis.networkanalysis import *
+#♣from qgis.networkanalysis import *
+from qgis import processing
 
 # Get the layers.
 lyrConsumer = processing.getObject(Consumer_Centroid_Layer)
@@ -45,19 +46,19 @@ builder = QgsGraphBuilder(crs)
 # New fields are simply the ID of the Centre.
 # Loop through each Centre to construct new field names.
 lyrOutput = processing.getObject(Output_Layer)
-provider = lyrOutput.dataProvider() 
+provider = lyrOutput.dataProvider()
 
 # Optimize feature request for this loop.
 request1 = QgsFeatureRequest().setFlags(QgsFeatureRequest.NoGeometry).setSubsetOfAttributes([Centre_Point_Layer_ID_Field], lyrCentre.fields() )
 
 # The loop.
 for centreFeature in lyrCentre.getFeatures(request1):
-    
+
     # Capture value of fldCentreID_index (current ID).
     currentCentreID = centreFeature[fldCentreID_index]
 
-    # Add and name the field. Double type for distances.   
-    new_field_name = currentCentreID    
+    # Add and name the field. Double type for distances.
+    new_field_name = currentCentreID
     provider.addAttributes([QgsField(new_field_name, QVariant.Double)])
     lyrOutput.updateFields()
 
@@ -66,34 +67,34 @@ lyrOutput.startEditing()
 
 # Optimize feature request for outer nested loop.
 request2 = QgsFeatureRequest().setSubsetOfAttributes([Consumer_Centroid_Layer_ID_Field], lyrConsumer.fields() )
-   
+
 # Loop through each Consumer feature.
 for consumerFeature in lyrConsumer.getFeatures(request2):
-    
+
     # Capture value of fldConsumerID_index (current ID).
     currentConsumerID = consumerFeature[fldConsumerID_index]
-    
+
     # Loop through each Centre feature.
     for centreFeature in lyrCentre.getFeatures():
-        
+
         # Setup the graph analysis.
         director = QgsLineVectorLayerDirector(lyrNetwork, -1, '', '', '', 3)
         properter = QgsDistanceArcProperter()
         director.addProperter(properter)
         crs = lyrConsumer.crs()
         builder = QgsGraphBuilder(crs)
-        
+
         # Capture value of fldCentreID_index (current ID).
         currentCentreID = centreFeature[fldCentreID_index]
-        
+
         # Get the path start point (Consumer point location).
         consumerGeom = consumerFeature.geometry()
         pStart = QgsPoint(consumerGeom.asPoint().x(),consumerGeom.asPoint().y())
-        
+
         # Get the path end point (Centre point location).
         centreGeom = centreFeature.geometry()
         pStop = QgsPoint(centreGeom.asPoint().x(),centreGeom.asPoint().y())
-        
+
         # Build the graph.
         tiedPoints = director.makeGraph(builder, [pStart, pStop])
         graph = builder.graph()
@@ -123,18 +124,18 @@ for consumerFeature in lyrConsumer.getFeatures(request2):
 
             # Create a new feature and put the shortest path polyline in it.
             # Then get the length of that line.
-    
+
             ftShortestPath = QgsFeature()
             ftShortestPath.setGeometry(QgsGeometry.fromPolyline(p))
             shortestPathNetworkDistance = ftShortestPath.geometry().length()
-            
-            # Set the network distance value of the new Centre field for 
+
+            # Set the network distance value of the new Centre field for
             # the current Consumer and Centre.
             lyrOutput.startEditing()
             current_distmatrix_field = currentCentreID
             distmatrix_field_index = lyrOutput.fieldNameIndex(current_distmatrix_field)
             lyrOutput.changeAttributeValue(consumerFeature.id(), distmatrix_field_index,shortestPathNetworkDistance)
             lyrOutput.commitChanges()
-            
+
 # Commit the changes to the layer.
 lyrOutput.commitChanges()
